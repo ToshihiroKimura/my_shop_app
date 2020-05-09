9788b4d1384ac1db87311cb4121a97815c58dee2
ToshihiroKimura <toshi.kimura5903@gmail.com>
2020-05-10 01:02:46 +0900

    root_path

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_174342) do
+ActiveRecord::Schema.define(version: 2020_05_08_175349) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postcode"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2020_05_08_174342) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
+  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "image", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_images_on_item_id"
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -88,5 +96,6 @@ ActiveRecord::Schema.define(version: 2020_05_08_174342) do
   add_foreign_key "addresses", "profiles"
   add_foreign_key "addresses", "users"
   add_foreign_key "cards", "users"
+  add_foreign_key "images", "items"
   add_foreign_key "profiles", "users"
 end
